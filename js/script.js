43ef0e8520713f1fb3910f507962060adae2d2f2
Angus Macrae <angusdmacrae@gmail.com>
2021-12-09 17:33:42 +0000

    Rename thankyou page and enable javascript

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const orderForm = document.querySelector('#order-form');
+const orderForm = document.getElementById('#order-form');
+
 orderForm.addEventListener('submit', async function submitForm(event) {
   event.preventDefault();
 
   const formData = new FormData(event.target);
-  console.log(formData);
   const processedData = new URLSearchParams(formData).toString();
   console.log(processedData);
 
-  // try {
-  //   await fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: new URLSearchParams(formData).toString(),
-  //   });
-  //   history.push('/thankyou.html');
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    await fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: processedData,
+    });
+    history.push('/thankyou.html');
+  } catch (error) {
+    console.log(error);
+  }
 });
