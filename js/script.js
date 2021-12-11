680120fe47b81bc4e90a218a48911dcf503f34f8
Angus Macrae <angusdmacrae@gmail.com>
2021-12-11 11:40:43 +0000

    Update selected products list and total cost on user input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const orderForm = document.getElementById("order-form");
+const selectedProductList = document.querySelector(".selected-product ul");
+const totalAmount = document.getElementById("amount");
 const jkoTransfer = document.getElementById("jko-transfer");
 const bankTransfer = document.getElementById("bank-transfer");
 const jkoImg = document.getElementById("jko-pay-img");
+const productInputs = document.querySelectorAll('.product-list input');
 
 jkoTransfer.addEventListener("click", function () {
   jkoImg.classList.remove("hidden");
@@ -29,3 +32,18 @@ orderForm.addEventListener("submit", async function submitForm(event) {
     console.log(error);
   }
 });
+
+productInputs.forEach((input, index) => input.addEventListener('change', function () {
+  const allProducts = [...productInputs].map(input => {
+    return { name: input.dataset.caption, unit: input.dataset.unit, cost: input.dataset.cost, quantity: Number(input.value) || 0 }
+  });
+  const selectedProducts = allProducts.filter(product => product.quantity > 0);
+
+  const totalCost = selectedProducts.reduce((acc, obj) => acc + obj.cost * obj.quantity, 0);
+  totalAmount.innerHTML = "$" + totalCost;
+
+  selectedProductList.innerHTML = selectedProducts.map(product => `
+  <li>
+    <p>${product.name} - $${product.cost}/${product.unit} X ${product.quantity} = ${product.cost * product.quantity}</p>
+  </li>`).join('');
+}));
