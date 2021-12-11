5f33b08e12c4e32649bca001a152e534c612b20d
Angus Macrae <angusdmacrae@gmail.com>
2021-12-11 11:54:23 +0000

    Update total cost when user selects different delivery cost

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const orderForm = document.getElementById("order-form");
 const selectedProductList = document.querySelector(".selected-product ul");
+const deliveryMethodSelect = document.querySelector("select.delivery-method");
 const totalAmount = document.getElementById("amount");
 const jkoTransfer = document.getElementById("jko-transfer");
 const bankTransfer = document.getElementById("bank-transfer");
 const jkoImg = document.getElementById("jko-pay-img");
 const productInputs = document.querySelectorAll('.product-list input');
+
+let allProducts = [];
+let selectedProducts = [];
 
 jkoTransfer.addEventListener("click", function () {
   jkoImg.classList.remove("hidden");
@@ -33,17 +37,30 @@ orderForm.addEventListener("submit", async function submitForm(event) {
   }
 });
 
+deliveryMethodSelect.addEventListener('change', function () {
+  updateTotalCost();
+});
+
 productInputs.forEach((input, index) => input.addEventListener('change', function () {
-  const allProducts = [...productInputs].map(input => {
-    return { name: input.dataset.caption, unit: input.dataset.unit, cost: input.dataset.cost, quantity: Number(input.value) || 0 }
+  updateTotalCost();
+  updateSelectedProducts();
+}));
+
+function updateTotalCost () {
+  allProducts = [...productInputs].map(input => {
+    return { name: input.dataset.caption, unit: input.dataset.unit, cost: Number(input.dataset.cost), quantity: Number(input.value) || 0 }
   });
-  const selectedProducts = allProducts.filter(product => product.quantity > 0);
+  selectedProducts = allProducts.filter(product => product.quantity > 0);
 
-  const totalCost = selectedProducts.reduce((acc, obj) => acc + obj.cost * obj.quantity, 0);
+  const deliveryCost = Number(deliveryMethodSelect.querySelector("option:checked").dataset.cost);
+
+  const totalCost = deliveryCost + selectedProducts.reduce((acc, obj) => acc + obj.cost * obj.quantity, 0);
   totalAmount.innerHTML = "$" + totalCost;
+}
 
+function updateSelectedProducts () {
   selectedProductList.innerHTML = selectedProducts.map(product => `
   <li>
     <p>${product.name} - $${product.cost}/${product.unit} X ${product.quantity} = ${product.cost * product.quantity}</p>
   </li>`).join('');
-}));
+}
